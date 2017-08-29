fa1f4149a7d3277b2112526eff3e9bedd2656660
moliva <miguel.oliva@mulesoft.com>
2017-08-29 11:41:47 -0300

    Git review: Adding rm for untracked files

--- utils/checkout-confirmation.sh
+++ utils/checkout-confirmation.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-read -n 1 -r -p "Reseting file \"$CURRENT_EDITING_FILE\"? [y/N] " input
+read -n 1 -r -p "Discarding changes for file \"$CURRENT_EDITING_FILE\"? [y/N] " input
 
 case $input in
 	[yY][eE][sS]|[yY])
-		git checkout -- "$CURRENT_EDITING_FILE"
+		if $IS_CURRENTLY_EDITING_FILE_UNTRACKED; then
+			rm "$CURRENT_EDITING_FILE"
+		else # tracked
+			git checkout -- "$CURRENT_EDITING_FILE"
+		fi
 		;;
 	*)
+		# take anything else as a no
 		;;
 esac
